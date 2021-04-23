46923941f035d88e43a58e4bbd8f69f5e0a9c7d1
IvanGarciaRubio <ivan.garcia@narancobrands.com>
2021-04-23 13:35:10 +0200

    modify

--- src/app/components/admin/dashboard/administrar-propiedades/administrar-propiedades.component.ts
+++ src/app/components/admin/dashboard/administrar-propiedades/administrar-propiedades.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Router, ActivatedRoute, Params} from '@angular/router';
-import {PropiedadService} from '../../../../services/propiedad.service';
-import {Owned} from '../../../../models/owned';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { PropiedadService } from '../../../../services/propiedad.service';
+import { Owned } from '../../../../models/owned';
 import { GLOBAL } from '../../../../services/global';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
@@ -17,15 +17,15 @@ export class AdministrarPropiedadesComponent {
   propertyForm = new FormGroup({
     nombre: new FormControl(''),
     descripcion: new FormControl(''),
-    personas: new FormControl(''),    
-    access: new FormControl('true'),
-    salas_reuniones: new FormControl('true'),
-    reception: new FormControl('true'),
-    eventos_network: new FormControl('true'),
-    terraza: new FormControl('true'),
-    cafe_relax: new FormControl('true'),
-    seguridad: new FormControl('true'),
-    limpieza: new FormControl('true'),
+    personas: new FormControl(''),
+    access: new FormControl(false),
+    salas_reuniones: new FormControl(false),
+    reception: new FormControl(false),
+    eventos_network: new FormControl(false),
+    terraza: new FormControl(false),
+    cafe_relax: new FormControl(false),
+    seguridad: new FormControl(false),
+    limpieza: new FormControl(false),
     tarifa: new FormControl(''),
     tipo_propiedad: new FormControl(''),
     imagen: new FormControl(''),
@@ -34,20 +34,66 @@ export class AdministrarPropiedadesComponent {
     comunidad_autonoma: new FormControl(''),
   });
 
-  constructor(private _route:ActivatedRoute,private _router:Router, private _propiedadService: PropiedadService, private fb: FormBuilder) { 
-    // this.propiedad= new Owned(0,'','',0,false,false,false,false,false,false,false,false,0,'','','','','');
+  constructor(private _route: ActivatedRoute, private _router: Router, private _propiedadService: PropiedadService, private fb: FormBuilder) {
   }
 
-  onSubmit(){
-    console.log(this.propertyForm.value);
-    
+  onSubmit() {
+    console.log(this.propertyForm.get('access').value);
+    if (this.propertyForm.get('access').value == true) {
+      this.propertyForm.get('access').setValue("true");
+    } else {
+      this.propertyForm.get('access').setValue("false");
+    }
+
+    if (this.propertyForm.get('salas_reuniones').value == true) {
+      this.propertyForm.get('salas_reuniones').setValue("true");
+    } else {
+      this.propertyForm.get('salas_reuniones').setValue("false");
+    }
+
+    if (this.propertyForm.get('reception').value == true) {
+      this.propertyForm.get('reception').setValue("true");
+    } else {
+      this.propertyForm.get('reception').setValue("false");
+    }
+
+    if (this.propertyForm.get('eventos_network').value == true) {
+      this.propertyForm.get('eventos_network').setValue("true");
+    } else {
+      this.propertyForm.get('eventos_network').setValue("false");
+    }
+
+    if (this.propertyForm.get('terraza').value == true) {
+      this.propertyForm.get('terraza').setValue("true");
+    } else {
+      this.propertyForm.get('terraza').setValue("false");
+    }
+
+    if (this.propertyForm.get('cafe_relax').value == true) {
+      this.propertyForm.get('cafe_relax').setValue("true");
+    } else {
+      this.propertyForm.get('cafe_relax').setValue("false");
+    }
+
+    if (this.propertyForm.get('seguridad').value == true) {
+      this.propertyForm.get('seguridad').setValue("true");
+    } else {
+      this.propertyForm.get('seguridad').setValue("false");
+    }
+
+    if (this.propertyForm.get('limpieza').value == true) {
+      this.propertyForm.get('limpieza').setValue("true");
+    } else {
+      this.propertyForm.get('limpieza').setValue("false");
+    }
     this._propiedadService.addPropiedad(this.propertyForm.value).subscribe(
       result => {
-        let json=JSON.stringify(result);
+        this.propiedad = this.propertyForm.value;
+        console.log(this.propiedad);
         this._router.navigate(['dashboard/listPropertys']);
       },
       error => {
-          console.log(<any>error);
+        console.log(<any>error);
       }
     );
   }
--- src/app/components/admin/dashboard/administrar-propiedades/editar-propiedades.component.ts
+++ src/app/components/admin/dashboard/administrar-propiedades/editar-propiedades.component.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {Router, ActivatedRoute, Params} from '@angular/router';
-import {PropiedadService} from '../../../../services/propiedad.service';
-import {Owned} from '../../../../models/owned';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { PropiedadService } from '../../../../services/propiedad.service';
+import { Owned } from '../../../../models/owned';
 import { GLOBAL } from '../../../../services/global';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-editar-propiedades',
-  templateUrl: './administrar-propiedades.component.html',
-  styleUrls: ['./administrar-propiedades.component.scss'],
-  providers: [PropiedadService]
+    selector: 'app-editar-propiedades',
+    templateUrl: './administrar-propiedades.component.html',
+    styleUrls: ['./administrar-propiedades.component.scss'],
+    providers: [PropiedadService]
 })
 export class EditarPropiedadesComponent {
     public propiedad: Owned;
-    public data:any;
+    public data: any;
 
     propertyForm = new FormGroup({
         nombre: new FormControl(''),
         descripcion: new FormControl(''),
-        personas: new FormControl(''),    
+        personas: new FormControl(''),
         access: new FormControl(''),
         salas_reuniones: new FormControl(''),
         reception: new FormControl(''),
@@ -35,58 +35,137 @@ export class EditarPropiedadesComponent {
         comunidad_autonoma: new FormControl(''),
     });
 
-    ngOnInit(){
+    ngOnInit() {
         this.recogerDato();
     }
 
-    recogerDato(){
+    recogerDato() {
         this._route.params.forEach((params: Params) => {
-            let id= params['id'];
+            let id = params['id'];
 
             this._propiedadService.getPropiedad(id).subscribe(
                 response => {
-                    this.propiedad=response;
-                    
-                    for(let i in this.propiedad){
+                    this.propiedad = response;
+
+                    for (let i in this.propiedad) {
                         this.propertyForm.get('nombre').setValue(this.propiedad[i].nombre);
                         this.propertyForm.get('descripcion').setValue(this.propiedad[i].descripcion);
                         this.propertyForm.get('personas').setValue(this.propiedad[i].personas);
-                        this.propertyForm.get('access').setValue(this.propiedad[i].access);
-                        this.propertyForm.get('salas_reuniones').setValue(this.propiedad[i].salas_reuniones);
-                        this.propertyForm.get('reception').setValue(this.propiedad[i].reception);
-                        this.propertyForm.get('eventos_network').setValue(this.propiedad[i].eventos_network);
-                        this.propertyForm.get('terraza').setValue(this.propiedad[i].terraza);
-                        this.propertyForm.get('cafe_relax').setValue(this.propiedad[i].cafe_relax);
-                        this.propertyForm.get('seguridad').setValue(this.propiedad[i].seguridad);
-                        this.propertyForm.get('limpieza').setValue(this.propiedad[i].limpieza);
+                        if (this.propiedad[i].access == "true") {
+                            this.propertyForm.get('access').setValue(true);
+                        } else {
+                            this.propertyForm.get('access').setValue(false);
+                        }
+                        if (this.propiedad[i].salas_reuniones == "true") {
+                            this.propertyForm.get('salas_reuniones').setValue(true);
+                        } else {
+                            this.propertyForm.get('salas_reuniones').setValue(false);
+                        }
+                        if (this.propiedad[i].reception == "true") {
+                            this.propertyForm.get('reception').setValue(true);
+                        } else {
+                            this.propertyForm.get('reception').setValue(false);
+                        }
+                        if (this.propiedad[i].eventos_network == "true") {
+                            this.propertyForm.get('eventos_network').setValue(true);
+                        } else {
+                            this.propertyForm.get('eventos_network').setValue(false);
+                        }
+                        if (this.propiedad[i].terraza == "true") {
+                            this.propertyForm.get('terraza').setValue(true);
+                        } else {
+                            this.propertyForm.get('terraza').setValue(false);
+                        }
+                        if (this.propiedad[i].cafe_relax == "true") {
+                            this.propertyForm.get('cafe_relax').setValue(true);
+                        } else {
+                            this.propertyForm.get('cafe_relax').setValue(false);
+                        }
+                        if (this.propiedad[i].seguridad == "true") {
+                            this.propertyForm.get('seguridad').setValue(true);
+                        } else {
+                            this.propertyForm.get('seguridad').setValue(false);
+                        }
+                        if (this.propiedad[i].limpieza == "true") {
+                            this.propertyForm.get('limpieza').setValue(true);
+                        } else {
+                            this.propertyForm.get('limpieza').setValue(false);
+                        }
                         this.propertyForm.get('tarifa').setValue(this.propiedad[i].tarifa);
                         this.propertyForm.get('tipo_propiedad').setValue(this.propiedad[i].tipo_propiedad);
                         this.propertyForm.get('direccion').setValue(this.propiedad[i].direccion);
                         this.propertyForm.get('ciudad').setValue(this.propiedad[i].ciudad);
                         this.propertyForm.get('comunidad_autonoma').setValue(this.propiedad[i].comunidad_autonoma);
                     }
-                }, error =>{
+                }, error => {
                     console.log(<any>error);
                 }
             );
         });
     }
 
-    constructor(private _route:ActivatedRoute,private _router:Router, private _propiedadService: PropiedadService, private fb: FormBuilder) { 
+    constructor(private _route: ActivatedRoute, private _router: Router, private _propiedadService: PropiedadService, private fb: FormBuilder) {
     }
 
-    onSubmit(){
+    onSubmit() {
         this._route.params.forEach((params: Params) => {
-            let id= params['id'];
-                this._propiedadService.editPropiedad(id,this.propertyForm.value).subscribe(
-                    result => {
-                        console.log(this.propertyForm.value);
-                        this._router.navigate(['/dashboard/listPropertys']);
-                    },
-                    error => {
-                        console.log(<any>error);
-                    }
-                );
+            let id = params['id'];
+            if (this.propertyForm.get('access').value == true) {
+                this.propertyForm.get('access').setValue("true");
+            } else {
+                this.propertyForm.get('access').setValue("false");
+            }
+
+            if (this.propertyForm.get('salas_reuniones').value == true) {
+                this.propertyForm.get('salas_reuniones').setValue("true");
+            } else {
+                this.propertyForm.get('salas_reuniones').setValue("false");
+            }
+
+            if (this.propertyForm.get('reception').value == true) {
+                this.propertyForm.get('reception').setValue("true");
+            } else {
+                this.propertyForm.get('reception').setValue("false");
+            }
+
+            if (this.propertyForm.get('eventos_network').value == true) {
+                this.propertyForm.get('eventos_network').setValue("true");
+            } else {
+                this.propertyForm.get('eventos_network').setValue("false");
+            }
+
+            if (this.propertyForm.get('terraza').value == true) {
+                this.propertyForm.get('terraza').setValue("true");
+            } else {
+                this.propertyForm.get('terraza').setValue("false");
+            }
+
+            if (this.propertyForm.get('cafe_relax').value == true) {
+                this.propertyForm.get('cafe_relax').setValue("true");
+            } else {
+                this.propertyForm.get('cafe_relax').setValue("false");
+            }
+
+            if (this.propertyForm.get('seguridad').value == true) {
+                this.propertyForm.get('seguridad').setValue("true");
+            } else {
+                this.propertyForm.get('seguridad').setValue("false");
+            }
+
+            if (this.propertyForm.get('limpieza').value == true) {
+                this.propertyForm.get('limpieza').setValue("true");
+            } else {
+                this.propertyForm.get('limpieza').setValue("false");
+            }
+            this._propiedadService.editPropiedad(id, this.propertyForm.value).subscribe(
+                result => {
+                    console.log(this.propertyForm.value);
+                    this._router.navigate(['/dashboard/listPropertys']);
+                },
+                error => {
+                    console.log(<any>error);
+                }
+            );
         });
     }
 }
